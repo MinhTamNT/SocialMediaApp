6494a80f0828b14daf38e6ce7ab8a5273e22cbaa
Minh Tâm <minhtam78945@gmail.com>
2023-10-24 16:47:17 +0700

    update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,8 @@
 import { Navbar } from '~/components/Nabar/Navbar'
-
+import { AiOutlineMenu } from 'react-icons/ai'
 export const Header = () => {
   return (
-    <div className='w-full h-[60px] bg-color-deafult border-[1px]  border-soild flex pointer-events-none border-boder-color-default border-b-sm'>
+    <div className='w-full h-[60px] bg-color-deafult border-[1px]  border-soild flex  border-boder-color-default border-b-sm'>
       <Navbar />
     </div>
   )
